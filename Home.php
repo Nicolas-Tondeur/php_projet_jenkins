83d42d57e5aa46a991506c3dd007554a7d14b98e
Nicolas-Tondeur <nicolastondeur@yahoo.fr>
2023-04-13 15:21:57 +0200

    5ème commit

--- Home.php
+++ Home.php
@@ -1,10 +1,11 @@
+<?php include 'Login.php' ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site de Recettes - Page d'accueil</title>
+    <title>Bonjour monsieur </title>
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
         rel="stylesheet"
